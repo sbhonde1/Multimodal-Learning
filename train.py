13f7a230d5ed530305d3021088ed865051e4ce09
sbhonde1 <sagarbhonde1@gmail.com>
2020-03-25 17:06:13 -0400

    ssa loss solved 1. Need to verify

--- train.py
+++ train.py
@@ -22,8 +22,8 @@ img_x, img_y = 256, 256  # resize video 2d frame size
 depth_x, depth_y = 320, 240
 # Select which frame to begin & end in videos
 begin_frame, end_frame, skip_frame = 1, 10, 1
-learning_rate = 1e-4
-_lambda = 0.05 # 50 x 10^-3
+lr = 1e-4
+_lambda = 0.05  # 50 x 10^-3
 
 
 def train(args,
@@ -35,13 +35,16 @@ def train(args,
           valid_loader,
           criterion,
           regularizer,
-          corr,
-          epoch):
+          epoch,
+          tq):
     device = args.device
     model_rgb.train()
     model_depth.train()
-    losses = []
-    scores = []
+    ssa_losses = []
+    rgb_losses = []
+    depth_losses = []
+    rgb_regularized_losses = []
+    depth_regularized_losses = []
 
     for batch_idx, (rgb, depth, y) in enumerate(train_loader):
         # distribute data to device
@@ -55,27 +58,27 @@ def train(args,
 
         rgb_feature_map_T = torch.transpose(rgb_feature_map, 1, 2)
         depth_feature_map_T = torch.transpose(depth_feature_map, 1, 2)
-        print("RGB fmap transpose shape :: {}".format(rgb_feature_map_T.shape))
-        print("depth fmap transpose shape :: {}".format(depth_feature_map_T.shape))
+        # print("RGB fmap transpose shape :: {}".format(rgb_feature_map_T.shape))
+        # print("depth fmap transpose shape :: {}".format(depth_feature_map_T.shape))
 
         rgb_corr = torch.mul(rgb_feature_map, rgb_feature_map_T)
         depth_corr = torch.mul(depth_feature_map, depth_feature_map_T)
-        print("RGB correlation ::  {}".format(rgb_corr.shape))
-        print("depth correlation :: {}".format(depth_corr.shape))
+        # print("RGB correlation ::  {}".format(rgb_corr.shape))
+        # print("depth correlation :: {}".format(depth_corr.shape))
 
         loss_rgb = criterion(rgb_out, torch.max(y, 1)[1])  # index of the max log-probability
         loss_depth = criterion(depth_out, torch.max(y, 1)[1])
 
-        print("RGB loss :: {}".format(loss_rgb))
-        print("depth loss :: {}".format(loss_depth))
+        # print("RGB loss :: {}".format(loss_rgb))
+        # print("depth loss :: {}".format(loss_depth))
 
         focal_reg_param = regularizer(loss_rgb, loss_depth)
 
-        print("focal regularizer parameter :: {} ".format(focal_reg_param))
+        # print("focal regularizer parameter :: {} ".format(focal_reg_param))
 
-        ssa_loss = focal_reg_param * (torch.sub(rgb_corr, depth_corr))
+        ssa_loss = focal_reg_param * torch.sum((torch.sub(rgb_corr, depth_corr)) ** 2)
 
-        print("ssa loss :: {} " .format(ssa_loss.shape))
+        # print("ssa loss :: {} " .format(ssa_loss.shape))
 
         reg_loss_rgb = loss_rgb + (_lambda * ssa_loss)
         reg_loss_depth = loss_depth + (_lambda * ssa_loss)
@@ -89,11 +92,22 @@ def train(args,
         optimizer_rgb.step()
         optimizer_depth.step()
 
-        # losses.append(loss_rgb.item())
-        if epoch % 10 == 0 and batch_idx == 0:
-            print("Regularizer : {}".format(focal_reg_param))
-            print("Normal RGB loss : {} \t Regularized RGB loss : {} ".format(loss_rgb, reg_loss_rgb))
-            print("Normal Depth loss : {} \t Regularized Depth loss : {} ".format(loss_depth, reg_loss_depth))
+        ssa_losses.append(ssa_loss.item())
+        rgb_losses.append(loss_rgb.item())
+        depth_losses.append(loss_depth.item())
+        rgb_regularized_losses.append(reg_loss_rgb.item())
+        depth_regularized_losses.append(reg_loss_depth.item())
+        tq.update(1)
+        # if epoch % 10 == 0 and batch_idx == 0:
+        #     print("Regularizer : {}".format(focal_reg_param))
+        #     print("Normal RGB loss : {} \t Regularized RGB loss : {} ".format(loss_rgb, reg_loss_rgb))
+        #     print("Normal Depth loss : {} \t Regularized Depth loss : {} ".format(loss_depth, reg_loss_depth))
+    mean_ssa = np.mean(ssa_losses)
+    mean_rgb = np.mean(rgb_losses)
+    mean_reg_rgb = np.mean(rgb_regularized_losses)
+    mean_depth = np.mean(depth_losses)
+    mean_reg_depth = np.mean(depth_regularized_losses)
+    return mean_ssa, mean_rgb, mean_reg_rgb, mean_depth, mean_reg_depth
 
 
 def main():
@@ -120,16 +134,16 @@ def main():
 
     # model_rgb_cnn = CNN3D(t_dim=len(selected_frames), img_x=img_x, img_y=img_y, num_classes=11).to(device)
     model_rgb_cnn = I3D(num_classes=11,
-                 modality='rgb',
-                 dropout_prob=0,
-                 name='inception').to(device)
-    # model_depth_cnn = CNN3D(t_dim=len(selected_frames), img_x=depth_x, img_y=depth_y, num_classes=11).to(device)
-    model_depth_cnn = I3D(num_classes=11,
                         modality='rgb',
                         dropout_prob=0,
                         name='inception').to(device)
-    optimizer_rgb = torch.optim.Adam(model_rgb_cnn.parameters(), lr=learning_rate)  # optimize all cnn parameters
-    optimizer_depth = torch.optim.Adam(model_depth_cnn.parameters(), lr=learning_rate)  # optimize all cnn parameters
+    # model_depth_cnn = CNN3D(t_dim=len(selected_frames), img_x=depth_x, img_y=depth_y, num_classes=11).to(device)
+    model_depth_cnn = I3D(num_classes=11,
+                          modality='rgb',
+                          dropout_prob=0,
+                          name='inception').to(device)
+    optimizer_rgb = torch.optim.Adam(model_rgb_cnn.parameters(), lr=lr)  # optimize all cnn parameters
+    optimizer_depth = torch.optim.Adam(model_depth_cnn.parameters(), lr=lr)  # optimize all cnn parameters
     criterion = torch.nn.CrossEntropyLoss()
     args = parse()
     args.device = device
@@ -140,36 +154,23 @@ def main():
             return (beta * math.exp(loss1 - loss2)) - 1
         return 0.0
 
-    def get_correlation_matrix(feature_map):    # Unused
-        variance, sample_mean = torch.var_mean(feature_map)
-        # x = torchvision.transforms.Normalize(mean=sample_mean, std=variance)
-        # normalized = x(feature_map)
-        # print(normalized.shape)
-        # sub_tensor = torch.sub(feature_map, sample_mean)
-        # normalized_tensor = torch.addcdiv(sub_tensor, variance)
-        # feature_map = feature_map.sub_(sample_mean).div_(variance)
-        sample_mean = torch.as_tensor(sample_mean, dtype=feature_map.dtype, device=feature_map.device)
-        variance = torch.as_tensor(variance, dtype=feature_map.dtype, device=feature_map.device)
-        feature_map.sub_(sample_mean).div_(variance)
-        return feature_map
-
     # print(model_rgb_cnn)
 
-
     for epoch in range(1):
-
-        train(args=args,
-              model_rgb=model_rgb_cnn,
-              model_depth=model_depth_cnn,
-              optimizer_rgb=optimizer_rgb,
-              optimizer_depth=optimizer_depth,
-              train_loader=train_loader,
-              valid_loader=valid_loader,
-              criterion=criterion,
-              regularizer=regularizer,
-              corr=get_correlation_matrix,
-              epoch=epoch)
-
+        tq = tqdm(total=(len(train_loader)))
+        tq.set_description('Epoch {}, lr {}'.format(epoch, lr))
+        ssa, rgb, reg_rgb, depth, reg_depth = train(args=args,
+                                                    model_rgb=model_rgb_cnn,
+                                                    model_depth=model_depth_cnn,
+                                                    optimizer_rgb=optimizer_rgb,
+                                                    optimizer_depth=optimizer_depth,
+                                                    train_loader=train_loader,
+                                                    valid_loader=valid_loader,
+                                                    criterion=criterion,
+                                                    regularizer=regularizer,
+                                                    epoch=epoch,
+                                                    tq=tq)
+        tq.set_postfix(ssa_loss='{:.5f}'.format(ssa), regularized_rgb_loss='{:.5f}'.format(reg_rgb))
 
 if __name__ == "__main__":
     main()
